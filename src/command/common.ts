d19a2507fea47e0ec7efc256c909c7cf65804eff
hbq <hbqhbq123@qq.com>
2017-05-07 11:17:38 +0800

    common.ts

--- src/command/common.ts
+++ src/command/common.ts
@@ -3,48 +3,53 @@ import * as  stringify from 'json-stringify-pretty-compact'
 import { prompt } from 'prompt-promise2'
 import * as  _ from 'lodash'
 import * as  chalk from 'chalk'
+import spawn from 'spawn-helper'
 /**
  * 公共属性及方法
  */
-export default {
-    templateFilePath: ioHelper.pathTool.join(__dirname, '../../', 'templates.json'),
-    rootPath: ioHelper.pathTool.join(__dirname, '..'),
-    getTemplate() {
-        return require(this.templateFilePath)
-    },
-    getTemplateString() {
-        return stringify(this.getTemplate())
-    },
-    async showTemplate() {
-        console.log('\n')
-        console.log(`***************************`)
-        console.log(chalk.green(await this.getTemplateString()))
-        console.log(`***************************`)
-        console.log('\n')
-    },
-    stringify(obj) {
-        return stringify(obj);
-    },
-    async prompt(describe) {
-        let value = await prompt(describe)
-        return _.trim(value)
-    },
-    async writeFile(path, content) {
-        return await ioHelper.writeFile(path, content)
-    },
-    async writeTemplate(content) {
-        content = _.isString(content) ? content : this.stringify(content)
-        return await this.writeFile(this.templateFilePath, content)
-    },
-    exit() {
-        process.exit()
-    },
-    showError(e) {
-        throw e
-        // console.error(e.stack)
-        // console.error(e.message)
-    },
-    urlResolve(url) {
-        return url.replace(/[\u0000-\u0019]/g, '')
-    }
+export function exec(cmd: string, opt?: any) {
+    return spawn.exec(cmd, opt)
 }
+export const templateFilePath = ioHelper.pathTool.join(__dirname, '../../', 'templates.json')
+export const rootPath = ioHelper.pathTool.join(__dirname, '..')
+export function getTemplate() {
+    return require(templateFilePath)
+}
+export function getTemplateString() {
+    return stringify(getTemplate())
+}
+export async function showTemplate() {
+    console.log('\n')
+    console.log(`***************************`)
+    console.log(chalk.green(await getTemplateString()))
+    console.log(`***************************`)
+    console.log('\n')
+}
+export function stringify(obj) {
+    return stringify(obj);
+}
+export async function prompt(describe) {
+    let value = await prompt(describe)
+    return _.trim(value)
+}
+export async function writeFile(path, content) {
+    return await ioHelper.writeFile(path, content)
+}
+export async function writeTemplate(content) {
+    content = _.isString(content) ? content : stringify(content)
+    return await writeFile(templateFilePath, content)
+}
+export function exit() {
+    process.exit()
+}
+export function showError(e) {
+    throw e
+    // console.error(e.stack)
+    // console.error(e.message)
+}
+export function urlResolve(url) {
+    return url.replace(/[\u0000-\u0019]/g, '')
+}
+
+
+export default { exec, templateFilePath, rootPath, getTemplate, getTemplateString, showTemplate, stringify, prompt, writeFile, writeTemplate, exit, showError, urlResolve }
